a1fb43a513f605265b35cce36c4cb76383b219de
sisodiaumang <sisodiaumang6@gmail.com>
2026-01-29 22:36:26 +0530

    Time: 14 ms (5.25%) | Memory: 17.7 MB (5.01%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,13 +5,9 @@ public:
         string part;
         for(int i=0;i<strs[0].size();i++){
             for(int j=1;j<strs.size();j++){
-                if(strs[0].substr(0,i+1)==strs[j].substr(0,i+1)){
-                    
-                }
-                else{
+                if(strs[0].substr(0,i+1)!=strs[j].substr(0,i+1)){
                     return ans;
                 }
-                part=strs[0].substr(0,i+1);
             }
             ans=ans+strs[0][i];
         }
