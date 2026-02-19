40a2ecf3ac91e038dee3156f92a27930a79aec0a
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-19 23:31:05 +0530

    Time: 15 ms (31.51%) | Memory: 14 MB (63.91%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -4,13 +4,15 @@ public:
         sort(nums.begin(),nums.end());
         int sum;
         int size=nums.size()-1;
-        int current=999999;
+        long long current=INT_MAX;
         for(int i=0;i<size-1;i++){
             int front=i+1;
-        
         int back=size;
             while(front<back){
                 sum=nums[i]+nums[back]+nums[front];
+                if(abs(target - sum) < abs(target - current)) {
+                    current = sum;
+                }
                 if(target==sum) return sum;
                 else if(target>sum){
                     front++;
@@ -18,12 +20,8 @@ public:
                 else{
                     back--;
                 }
-                if(abs(target - sum) < abs(target - current)) {
-            current = sum;
-            }}
+                }
             }
-        
-
         return current;
     }
 };
