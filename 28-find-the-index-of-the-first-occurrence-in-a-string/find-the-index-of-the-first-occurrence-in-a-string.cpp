1e8172699910f40e0a55fc99e508f0fcad1935f8
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-20 23:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (44.52%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,14 +3,37 @@ public:
     int strStr(string haystack, string needle) {
         int size=haystack.size();
         int nsize=needle.size();
-
-        for(int i=0;i<size-nsize+1;i++){
-            if(needle==haystack.substr(i,nsize)){
-                return i;
-            };
+        int p1=0,p2=0;
+        int t=0;
+        while(p1!=size){
+            if(haystack[p1]==needle[p2]){
+                p1++;p2++;
+                if(p2==nsize) return t;
+            }
+            else{
+                p1=t+1;
+                p2=0;
+                t=p1;
+            }
         }
         return -1;
 
-
     }
 };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int size=haystack.size();
+//         int nsize=needle.size();
+
+//         for(int i=0;i<size-nsize+1;i++){
+//             if(needle==haystack.substr(i,nsize)){
+//                 return i;
+//             };
+//         }
+//         return -1;
+
+
+//     }
+// };
