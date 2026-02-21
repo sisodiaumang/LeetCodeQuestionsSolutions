6fd5fb8a5068f1e1752315ebd3dea35c2329577d
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-21 23:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.09%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int not_unique=0;
-
         int size=nums.size();
-        vector<int> box;
-
-        for(int i=0;i<size;i++){
-            if(find(box.begin(), box.end(),nums[i])==box.end()){
-                box.push_back(nums[i]);
+        if(size==0) return 0;
+        int ptr2=0;
+        for(int i=1;i<size;i++){
+            if(nums[i]!=nums[ptr2]){
+                ptr2++;
+                nums[ptr2]=nums[i];
             }
-            else{
-                not_unique++;
-            }
-            
         }
-        nums=box;
-        return size-not_unique;
+
+        return ++ptr2;
     }
 };
