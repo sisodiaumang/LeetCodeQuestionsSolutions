d5b26ab23a82cf7e1e75370ee6ba2a666a285cbe
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-23 23:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (48.12%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -3,14 +3,18 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int size=nums.size();
         int count=0;
-
+        int index=0;
         for(int i=0;i<size;i++){
-            if(nums[i]==val){
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
+            }
+            else{
                 count++;
-                nums[i]=INT_MAX;
             }
         }
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        
         return size-count;
     }
 };
