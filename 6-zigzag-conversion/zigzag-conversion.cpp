d2bc18a601a03e530deccb71cde1afe41455ad9e
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-25 23:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (93.81%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int size=s.size();
+        if(numRows==1||size==numRows) return s;
+        string ans="";
+        int index=0;
+        int cycle = 2 * (numRows - 1);
+        for(int i=0;i<numRows;i++){
+            for(int r=i;r<size;r+=cycle){
+                ans+=s[r];
+                int second = r + cycle - 2 * i;
+
+                if (i != 0 && i != numRows - 1 && second < size) {
+                    ans += s[second];
+                }
+                }
+            }
+
+        return ans;
+    }
+};
