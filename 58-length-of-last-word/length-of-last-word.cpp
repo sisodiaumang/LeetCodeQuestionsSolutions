e3061629e4342889fadeaa59e2b24234267bbd77
Umang Sisodia <sisodiaumang6@gmail.com>
2026-02-26 22:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (32.97%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int c=0;
+        int size=s.size();
+        bool last=false;
+        while(size > 0 && s[size - 1] == ' '){
+            size--;
+        }
+        for(int i=size-1;i>=0;i--){
+            if(s[i]==' '){
+                break;
+            }
+            c++;
+        }
+        return c;
+
+    }
+};
