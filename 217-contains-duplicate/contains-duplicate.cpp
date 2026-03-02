00acb9107722a6729e96b7e34ada05e71c25cf1f
Umang Sisodia <sisodiaumang6@gmail.com>
2026-03-02 19:29:28 +0530

    Time: 23 ms (86.53%) | Memory: 71 MB (78.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -5,7 +5,7 @@ public:
         int size=nums.size();
         if(size==1) return false;
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<size;i++){
             if(nums[i-1]==nums[i]){
                 return true;
             }
