9d9c51c71ddb6844b7904672c5400fb203e8597f
Umang Sisodia <sisodiaumang6@gmail.com>
2026-03-02 19:41:41 +0530

    Time: 18 ms (95.19%) | Memory: 71.1 MB (78.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-      
-
-    unordered_set<int> s;
-
-    for (int num : nums) {
-        if (s.count(num)) return true;
-        s.insert(num);
-    }
-    return false;
-}
-};
-
-
 // class Solution {
 // public:
 //     bool containsDuplicate(vector<int>& nums) {
-//         int size=nums.size();
-//         if(size==1) return false;
-//         sort(nums.begin(),nums.end());
-//         for(int i=1;i<size;i++){
-//             if(nums[i-1]==nums[i]){
-//                 return true;
-//             }
-//         }
+      
 
-//         return false;
+//     unordered_set<int> s;
+
+//     for (int num : nums) {
+//         if (s.count(num)) return true;
+//         s.insert(num);
 //     }
-        
+//     return false;
+// }
 // };
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size=nums.size();
+        if(size==1) return false;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<size;i++){
+            if(nums[i-1]==nums[i]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+        
+};
