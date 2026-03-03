3f8e612791e426002965d807d22cef345cc8d18c
Umang Sisodia <sisodiaumang6@gmail.com>
2026-03-03 15:43:06 +0530

    Time: 71 ms (75.49%) | Memory: 98.8 MB (33.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> s;
+        unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++){
-            if(s.count(nums[i])){
-                return true;
+            if(m.find(nums[i]) != m.end()){
+                if(abs(m[nums[i]]-i)<=k){
+                    return true;
+                }
             }
-            s.insert(nums[i]);
-            if(s.size()>k){
-                s.erase(nums[i - k]);
-            }
+            m[nums[i]]=i;  
         }
         return 0;
     }
     
 };
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_set<int> s;
+//         for(int i=0;i<nums.size();i++){
+//             if(s.count(nums[i])){
+//                 return true;
+//             }
+//             s.insert(nums[i]);
+//             if(s.size()>k){
+//                 s.erase(nums[i - k]);
+//             }
+//         }
+//         return 0;
+//     }
+    
+// };
