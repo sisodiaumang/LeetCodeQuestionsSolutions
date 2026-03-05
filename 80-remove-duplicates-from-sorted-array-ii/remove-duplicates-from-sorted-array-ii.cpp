af5c44e9afc62e7df6747f929f2512eccb09b905
Umang Sisodia <sisodiaumang6@gmail.com>
2026-03-05 17:54:12 +0530

    Time: 3 ms (95.86%) | Memory: 19.5 MB (81.27%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -9,8 +9,7 @@ public:
             if(nums[p2]!=nums[p1-2]){
                 nums[p1++] = nums[p2];
             }
-            p2++;
-            
+            p2++;  
         }
         return p1;
     }
